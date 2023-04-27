7f5176d2bd295b617672d5be6574ecf887d6b372
PeterMwambi <calebmwambi@gmail.com>
2023-04-27 20:29:34 +0300

    changes to public autoload file

--- app/public/autoload.php
+++ app/public/autoload.php
@@ -3,9 +3,9 @@
 use Models\Core\App\Utilities\Autoloader as Autoload;
 
 
-if (!file_exists(dirname(__DIR__) . "\models\core\app\utilities\autoloader.php")) {
+if (!file_exists(str_replace("\\", "/", dirname(__DIR__)) . "/models/core/app/utilities/autoloader.php")) {
     die;
 }
-require_once dirname(__DIR__) . "\models\core\app\utilities\autoloader.php";
+require_once str_replace("\\", "/", dirname(__DIR__)) . "/models/core/app/utilities/autoloader.php";
 
 Autoload::start();
